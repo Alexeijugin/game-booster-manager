3c9453bd71a40f87997cfb115782b5f1996f826f
Alexeijugin <57283269+Alexeijugin@users.noreply.github.com>
2026-08-14 08:40:18 +0300

    Add order profit calculation

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import sys
-print("Текущая версия проекта:", sys.version)
+def calculate_order(price, hours, commission_percent):
+    if hours <= 0:
+        raise ValueError("Часов должно быть больше чем 0")
+    if price <= 0:
+        raise ValueError("Цена должна быть больше чем 0")
+    if commission_percent < 0 or commission_percent > 100:
+        raise ValueError("Комиссия должна быть от 0 до 100")
+
+    commission = price / 100 * commission_percent
+    price_non_commission = price - commission
+    profit_for_hour = price_non_commission / hours
+
+    return price_non_commission, profit_for_hour
 
