b7d6131b578567d05c92b06cd181d0db745c08a3
Alexeijugin <57283269+Alexeijugin@users.noreply.github.com>
2026-08-15 15:38:41 +0300

    Add pytest fixture for statistics tests

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -2,20 +2,31 @@ from models.order import Order
 from services.statistics import calculate_statistics
 import pytest
 
-def test_calculate_statistics():
-    orders = [
+
+@pytest.fixture
+def orders():
+    return [
         Order(1000, 2.5, 10),
         Order(950, 3, 10),
         Order(1500, 4, 15),
         Order(700, 2, 0),
     ]
 
+
+def test_calculate_statistics(orders):
     total_profit, total_hours, profit_per_hour = calculate_statistics(orders)
 
     assert total_profit == 3730
     assert total_hours == 11.5
     assert profit_per_hour == pytest.approx(324.347826)
 
+
 def test_calculate_statistics_empty_orders():
     with pytest.raises(ValueError, match="Список заказов не должен быть пустым"):
         calculate_statistics([])
+
+
+def test_calculate_statistics_total_hours(orders):
+    _, total_hours, _ = calculate_statistics(orders)
+
+    assert total_hours == 11.5
